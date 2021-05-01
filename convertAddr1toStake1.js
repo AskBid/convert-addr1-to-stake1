4a449ade4c9a4bbd7036781e56c61b33dbad2087
Sergio Marelli <sergio.mrll@gmail.com>
2021-05-01 20:38:56 +0100

    add arguments from CLI

--- convertAddr1toStake1.js
+++ convertAddr1toStake1.js
@@ -1,9 +1,12 @@
 const cardano = require('@emurgo/cardano-serialization-lib-nodejs')
 // import {BaseAddress} from '@emurgo/cardano-serialization-lib-browser'
 
-function stake1_from_addr1() {
+let myArgs = process.argv.slice(2);
+
+
+function stake1_from_addr1(addr1) {
 	// Build base address
-	let addr = cardano.Address.from_bech32('addr1q9whnenj7f83wvqtqw756drkd85pynz2hxrxz8szd4k5yd9uxl00u6yewtjluzse4x8mpd9x0k6x6zjmtsxtaamrtyjq7f78vk')
+	let addr = cardano.Address.from_bech32('addr1')
 	let base_addr = cardano.BaseAddress.from_address(addr)
 
 	// Extract stake credential               
@@ -20,9 +23,11 @@ function stake1_from_addr1() {
 	reward_addr_bytes.set(stake_cred.to_bytes().slice(4, 32), 1)           
 	let reward_addr = cardano.RewardAddress.from_address(cardano.Address.from_bytes(reward_addr_bytes))
 
-	// Log bech32 address         
-	console.log(reward_addr.to_address().to_bech32())
+	// Log bech32 address  
+	const stake1 = reward_addr.to_address().to_bech32()       
+	console.log(stake1)
+	return stake1
 	// stake1ux7r0hh7dzvh9e07pgv6nraskjn8mdrdpfd4cr977a34jfquwqrj9
 }
 
-stake1_from_addr1()
+stake1_from_addr1(myArgs[0])
